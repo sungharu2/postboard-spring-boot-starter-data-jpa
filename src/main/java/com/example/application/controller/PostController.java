e16f1c766c4e5d29bfac8218f54e5c17ea7a3c7a
sungharu <sungharu2@naver.com>
2020-11-09 17:00:44 +0900

    변경 - 컨트롤러 기능 단순화
    DTO, 서비스와 연결
    Thymeleaf 설정 및 템플릿 구조 단순화

--- src/main/java/com/example/application/controller/PostController.java
+++ src/main/java/com/example/application/controller/PostController.java
@@ -3,38 +3,62 @@ package com.example.application.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.application.entity.Post;
-import com.example.application.repository.PostRepository;
+import com.example.application.dto.PostDto;
+import com.example.application.service.PostService;
 
-@RestController
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller
 public class PostController {
-	@Autowired
-	PostRepository postRepository;
+	private PostService postService;
 	
-	
-	@GetMapping("/post")
-	public List<Post> getAllPost() {
-		return postRepository.findAll();
+	// TODO
+	// 게시글 리스트 조회
+	@GetMapping("/")
+	public String list() {
+		//return postService.findAll();
+		return "board/list.html";
 	}
-	
+
+	// TODO
+	// 단일 게시글 조회
 	@GetMapping("/post/{id}")
-	public Post getPost(@PathVariable String id) {
+	public void getPost(@PathVariable String id) {
 		Long postId = Long.parseLong(id);
 		
-		Optional<Post> post = postRepository.findById(postId);
+		//Optional<PostEntity> post = postService.findById(postId);
 		
-		return post.get();
 	}
 	
+	// 게시글 생성
+	@PostMapping("/post")
+	public String createPost(PostDto postDto) {		// html에서 json form으로 데이터를 주지 않음, @RequestBody 어노테이션 사용 안 해야 인식
+		postService.savePost(postDto);
+		
+		return "redirect:/";
+	}
+	
+	// 게시글 삭제 (기능 미사용/보류)
+	/*
+	@DeleteMapping("/post/{id}")
+	public String deletePost(@PathVariable String id) {
+		Long postId = Long.parseLong(id);
+		postRepository.deleteById(postId);
+		
+		return "Delete Success";
+	} */
+
+	// 게시글 수정 (기능 미사용/보류)
+	/*
 	@PostMapping("/post/{id}")
-	public Post updatePost(@PathVariable String id, @RequestBody Post newPost) {
+	public PostEntity updatePost(@PathVariable String id, @RequestBody PostEntity newPost) {
 		Long postId = Long.parseLong(id);
 		
-		Optional<Post> post = postRepository.findById(postId);
+		Optional<PostEntity> post = postRepository.findById(postId);
 		
 		post.get().setTitle(newPost.getTitle());
 		post.get().setContent(newPost.getContent());
@@ -42,20 +66,6 @@ public class PostController {
 		postRepository.save(post.get());
 		
 		return post.get();
-	}
+	} */
 	
-	@PostMapping("/post")
-	public Post createPost(Post post) {		// html에서 json form으로 데이터를 주지 않음, @RequestBody 어노테이션 사용 안 해야 인식
-		Post newPost = postRepository.save(post);
-		
-		return newPost;
-	}
-	
-	@DeleteMapping("/post/{id}")
-	public String deletePost(@PathVariable String id) {
-		Long postId = Long.parseLong(id);
-		postRepository.deleteById(postId);
-		
-		return "Delete Success";
-	}
 }
