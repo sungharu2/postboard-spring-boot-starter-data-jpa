10e2c955fa5bf4f159186df32fbacd1e3b406119
sungharu <sungharu2@naver.com>
2020-11-09 17:25:39 +0900

    제거 - Post -> Board로 이름 변경
    Post는 HTTP Get, Post 메소드와 혼동될 가능성이 있어 모두 게시글을 가리키는 명칭을 Board 변경

--- src/main/java/com/example/application/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.application.entity.PostEntity;
-
-public interface PostRepository extends JpaRepository<PostEntity, Long>{
-
-}
